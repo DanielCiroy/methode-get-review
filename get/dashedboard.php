9acb8cf748745dc6c6eb75b77068022a327df9ef
Daniel ciroy <142291477+DanielCiroy@users.noreply.github.com>
2024-07-03 21:42:48 +0200

    get review

---
+++ get/dashedboard.php
@@ -0,0 +1,30 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD']=="POST"){
+
+    
+ $username=$pwd="";
+
+ if(isset($_POST['username']) && isset($_POST['pwd'])){
+    if(!empty($_POST['username']) && !empty($_POST['pwd'])){
+        $username= $_POST['username'];
+        $pwd=$_POST['pwd'];
+
+        echo $username ." : ".$pwd;
+    }else{
+        echo "veuillez cpompleter tout les champs";
+    }
+ }else{
+    echo "les variables n'existent pas";
+ }
+
+
+
+}
+
+
+
+ 
+
+
+?>
